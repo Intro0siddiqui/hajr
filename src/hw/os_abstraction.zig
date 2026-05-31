225ada7406e5bb0040484c95ddca6c5bd0e475b8
Intro0siddiqui <introsiddique@gmail.com>
2026-05-31 13:55:16 +0530

    fix(windows): add TokenOpenFailed to LockdownError set in os_abstraction

--- src/hw/os_abstraction.zig
+++ src/hw/os_abstraction.zig
@@ -325,6 +325,7 @@ pub const LockdownError = error{
     KernelTooOld,
     SandboxInitFailed,
     MitigationPolicyFailed,
+    TokenOpenFailed,
 };
 
 /// Seal the current process for all future operations.
